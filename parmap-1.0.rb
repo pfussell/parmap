c506c70045406d7533c8e347987072c3c8feda89
Patrick Fussell <patrick.fussell@gmail.com>
2018-06-19 10:05:59 -0700

    update

--- parmap-1.0.rb
+++ parmap-1.0.rb
@@ -1,189 +1,125 @@
-#!/usr/bin/env ruby
-
-require "nmap/xml"
-require "thor"
-require 'readline'
+require 'nmap/xml'
 
 
-# Takes an nmap xml file as an argument
-# and parses the results to various formats
-class Parsing < Nmap::XML
+class ParseScan < Nmap::XML
   LINE_SEPERATOR = "|" + "-" * 114 + "|"
-
+  EMPTY = " " * 42 + "|"
+  
   def initialize(xmlfi)
-    @xmlfi = Nmap::XML.open(xmlfi)
-    @host_hash = {}
-    hosts_hash # just an easy way to look at hosts/ports
+    @xmlfi = Nmap::XML.open(xmlfi) 
   end
 
-  def hosts_hash
-    ports_arry = []
+  ## PARSING METHODS
+  
+  # get a list of live hosts based on up status(ie. ping) 
+  # return the array of 'host' objects
+  def parse_up_hosts
+    @xmlfi.up_hosts
+  end
+
+  # get a list of live hosts based on the presence of open ports
+  # return the array of 'host' objects where the host has at least
+  # one open port
+  def parse_live_hosts
+    has_ports = []
     @xmlfi.each_host do |host|
-      @host_hash["host.ip"]
-      host.each do |port|
-        ports_arry << port if port
-        @host_hash[host] = ports_arry
-      end
+      host.each_open_port { |port| has_ports << host }
     end
+    return has_ports.uniq
   end
 
-  def show_livehosts
-    @host_hash.each_key { |key| puts key }
-  end
-
-  def up_hst
-    @xmlfi.up_hosts { puts host.ip }
-  end
-
-  def pretty_print
-    @host_hash.each_key do |host|
-      puts LINE_SEPERATOR
-      host.each do |port|
-        prod = "#{port.service.product.to_s.slice(0..34)} #{port.service.version.to_s.slice(0..24)}"
-        printf("| %-18s | %-6d | %-4s | %-16s | %-56s | \n", host.ip, port.number, port.protocol.upcase, port.service.to_s.upcase, prod)
-      end
-    end
-    puts LINE_SEPERATOR
-  end
-
-  def hostList
-    printf("| %-16s | %-30s | \n", "-"*16, "-"*30)
-    printf("| %-16s | %-30s | \n", " IP ", " HOSTNAME ")
-    printf("| %-16s | %-30s | \n", "-"*16, "-"*30)
-    @host_hash.each_key do |host|
-      printf("| %-16s | %-30s | \n", host.ip, host.hostname)
-    end
-  end
-
-  def write_out(prt)
-    outf = File.open("port_#{prt}.txt", "a+")
-
-    @host_hash.each_key do |host|
-      host.each_port do |port|
-        outf.write("#{host.ip}\n") if port.to_s == prt # prt needs to be a string
-      end
-    end
-    outf.close
-  end
-
-  def report_out(outfi)
-    out_file = File.open("#{outfi}", "w")
+  # get an array of host objects that have a given
+  # port open 
+  def hosts_with_port(check_port)
+    has_port = []
     @xmlfi.each_host do |host|
-      host.each do |port|
-        out_file << "#{host.ip},#{port.number},#{port.protocol},#{port.service}, #{port.service.product} #{port.service.version}\n"
+      host.each_open_port do |port|
+        has_port << host if port.to_i == check_port
       end
     end
+    return has_port
   end
 
-  def parse_nse
-    @xmlfi.each_host do |host|
-      puts "[#{host.ip}]"
 
-      host.scripts.each do |name,output|
-        output.each_line { |line| puts "  #{line}" }
+  ## OUTPUT METHODS
+
+  # test scan type and print live hosts accordingly
+  # if scan inlcudes a 'noping' rule we will only output
+  # hosts with open ports (see 'parse_live_hosts')
+  # TODO: out_type to select stdout or File for output 
+  def live_hosts_to_stdout(host_type = :ip, out_type = :std)
+    if host_type == :ip
+      if @xmlfi.scanner["arguments"].include?("-Pn")
+        parse_live_hosts.each {|host| puts host.ip}
+      else
+        parse_up_hosts.each {|host| puts host.ip}
       end
-
-      host.each_port do |port|
-        puts "  [#{port.number}/#{port.protocol}]"
-
-        port.scripts.each do |name,output|
-          puts "    [#{name}]"
-
-          output.each_line { |line| puts "      #{line}" }
+    # if we want to print by name we need to check
+    # if the host has a name otherwise still do by IP
+    elsif host_type == :name
+      if @xmlfi.scanner["arguments"].include?("-Pn")
+        parse_live_hosts.each do |host|
+          if host.hostname
+            puts host.hostname
+          else
+            puts host.ip
+          end
+        end
+      else
+        parse_up_hosts.each do |host|
+          if host.hostname
+            puts host.hostname
+          else
+            puts host.ip
+          end
         end
       end
     end
   end
 
-
-end
-
-def interactive
-
-end
-
-# Use thor to manage the command line options
-class Parmap < Thor
-  # option :file, :required => true
-  desc 'parse FILE', 'parse the FILE and output the results to the screen'
-  def parse_xml(file)
-    parser = Parsing.new(file)
-    parser.pretty_print
+  # print all hosts with given port
+  def output_by_port(selected_port)
+    hosts_with_port(selected_port).each { |host| puts host.ip }
   end
 
-  desc 'ports FILE PORT', 'create a file with a list of hosts where the port was open'
-  def ports(file, prt)
-    parser = Parsing.new(file)
-    parser.write_out(prt)
+  # quick parse method of ports
+  # create output files with list of IPs for some
+  # common ports
+  def quick_parse_by_port
+    # http
+    # ssh
+    # ftp
+    # smtp
+    # smb/netbios
+    # nfs
+    # sql
+    # nosql (redis,mongo)
   end
 
-  desc 'csv FILE OUTPUT_FILE', 'create a csv of the output from parsing the nmap file'
-  def csv(file, outfile)
-    parser = Parsing.new(file)
-    parser.report_out(outfile)
-  end
-
-  desc 'nse FILE', 'parse the NSE script data from an nmap scan'
-  def nse(file)
-    parser = Parsing.new(file)
-    parser.parse_nse
-  end
-
-  desc 'hosts FILE', 'print a list of Up hosts in the file'
-  def hosts(file)
-    parser = Parsing.new(file)
-    parser.show_livehosts
-  end
-
-  desc 'shell', 'run an interactive shell for reading nmap scan data'
-  def ishell(file) 
-    while input = Readline.readline("> ", true)
-      break if input == "exit"
-      
-      case input
-      when "hosts"
-        parser = Parsing.new(file)
-        parser.hostList
-      when "ports"
-        parser = Parsing.new(file)
-        parser.pretty_print    
+  # this needs some serious work
+  # need to detect if we have just service OR version scan and print
+  # accordingly
+  # and need to fix the formatting
+  # maybe give more printing options in final product (this is org table for now) 
+  def pretty_service_output
+    @xmlfi.each_host do |host|
+      puts LINE_SEPERATOR
+      host.each do |port|
+        unless port.service.product
+          printf("| %-18s | %-6d | %-4s | %-44s \n", host.ip, port.number, port.protocol.upcase, EMPTY)
+        else
+          prod = "#{port.service.product.to_s.slice(0..24)} #{port.service.version.to_s.slice(0..24)}"
+          printf("| %-18s | %-6d | %-4s | %-28s | %-44s | \n", host.ip, port.number, port.protocol.upcase,
+                 port.service.to_s.upcase, prod)
+        end
       end
-      
-      puts Readline::HISTORY.to_a if input == "history"
-
-      # Remove blank lines from history
-      Readline::HISTORY.pop if input == ""
-
-      system(input)
-    end  
-  end
-
-end
-
-Parmap.start(ARGV)
-
-def ishell(file)
-  parser = Parser.new(file)
-  
-  while input = Readline.readline("> ", true)
-    break if input == "exit"
-
-    case input
-    when "load"
-      
-    when "hosts"
-      parser = Parsing.new(file)
-      parser.hostList
-    when "ports"
-      parser = Parsing.new(file)
-      parser.pretty_print
     end
-
-    puts Readline::HISTORY.to_a if input == "history"
-
-    # Remove blank lines from history
-    Readline::HISTORY.pop if input == ""
-
-    system(input)
+    puts LINE_SEPERATOR
   end
+  
+
 end
+
+#puts ARGV[0]
+#test = ParseScan.new(ARGV[0])
+#test.live_hosts_to_stdout
