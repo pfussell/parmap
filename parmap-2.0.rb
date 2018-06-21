42846970dfb1c243b757dca9f1dd6f048afed5f3
Patrick Fussell <patrick.fussell@gmail.com>
2018-06-21 09:30:12 -0700

    updated and fixed code. added shell method.

--- parmap-2.0.rb
+++ parmap-2.0.rb
@@ -1,5 +1,5 @@
 require 'nmap/xml'
-
+require 'readline'
 
 class ParseScan < Nmap::XML
   LINE_SEPERATOR = "|" + "-" * 114 + "|"
@@ -29,15 +29,19 @@ class ParseScan < Nmap::XML
   end
 
   # get an array of host objects that have a given
-  # port open 
-  def hosts_with_port(check_port)
+  # port open
+  # because of the way ruby these objects you will
+  # likely have duplicates if you check multiple ports
+  def hosts_with_port(*check_port)
     has_port = []
-    @xmlfi.each_host do |host|
-      host.each_open_port do |port|
-        has_port << host if port.to_i == check_port
+    check_port.each do |targ_port|
+      @xmlfi.each_host do |host|
+        host.each_open_port do |port|        
+          has_port << host if port.to_i == targ_port
+        end
       end
     end
-    return has_port
+    has_port
   end
 
 
@@ -78,22 +82,76 @@ class ParseScan < Nmap::XML
   end
 
   # print all hosts with given port
-  def output_by_port(selected_port)
-    hosts_with_port(selected_port).each { |host| puts host.ip }
+  # first arugment has to be :std OR :file depending on what you want to do
+  # with the output
+  # the second argument is an array (can be many numbers) of ports to test for
+  def output_by_port(out_type = :std, *selected_port)
+    hosts_array = []
+    selected_port.each do |target_port|
+      hosts_with_port(target_port).each { |host| hosts_array << host.ip.to_s }
+    end
+    
+    if out_type == :std
+      puts hosts_array.uniq
+    elsif out_type == :file
+      return hosts_array.uniq
+    else
+      puts "No valid output type selected."
+    end
+    
   end
 
   # quick parse method of ports
   # create output files with list of IPs for some
   # common ports
   def quick_parse_by_port
-    # http
-    # ssh
-    # ftp
-    # smtp
-    # smb/netbios
-    # nfs
-    # sql
-    # nosql (redis,mongo)
+    # http (80,443, 8080)
+    http_f = File.open('hosts_http.txt', 'w')
+    output_by_port(:file, 80, 443, 8080).each { |final_host| http_f << "#{final_host}\n" }
+
+    # ssh (22)
+    ssh_f = File.open('hosts_ssh.txt' 'w')
+    output_by_port(:file, 22).each { |final_host| ssh_f << "#{final_host}\n" }
+    
+    # telnet (23)
+    telnet_f = File.open('hosts_telnet.txt' 'w')
+    output_by_port(:file, 111, 2049).each { |final_host| ssh_f << "#{final_host}\n" }
+    
+    # ftp (21)
+    ftp_f = File.open('hosts_ftp.txt' 'w')
+    output_by_port(:file, 21).each { |final_host| ftp_f << "#{final_host}\n" }
+    
+    # smtp (25, 465, 587)
+    smtp_f = File.open('hosts_smtp.txt' 'w')
+    output_by_port(:file, 25, 465, 587).each { |final_host| ssh_f << "#{final_host}\n" }    
+    
+    # smb/netbios (445, 139) 
+    smb_f = File.open('hosts_smb.txt', 'w')
+    output_by_port(:file, 445).each { |final_host| smb_f << "#{final_host}\n" }
+    
+    nbns_f = File.open('hosts_nbns.txt', 'w')
+    output_by_port(:file, 139).each { |final_host| nbns_f << "#{final_host}\n" }
+    
+    # nfs (111, 2049) 
+    nfs_f = File.open('hosts_nfs.txt', 'w')
+    output_by_port(:file, 111, 2049).each { |final_host| nfs_f << "#{final_host}\n" }
+    
+    # sql (1433, 3306, 5432, 1521) 
+    mssql_f = File.open('hosts_mssql.txt', 'w')
+    output_by_port(:file, 1433).each { |final_host| mssql_f << "#{final_host}\n" }
+    mysql_f = File.open('hosts_mysql.txt', 'w')
+    output_by_port(:file, 3306).each { |final_host| mysql_f << "#{final_host}\n" }
+    pgsql_f = File.open('hosts_pgsql.txt', 'w')
+    output_by_port(:file, 5432).each { |final_host| pgsql_f << "#{final_host}\n" }
+    osql_f = File.open('hosts_osql.txt', 'w')
+    output_by_port(:file, 1521).each { |final_host| osql_f << "#{final_host}\n" }
+
+    # nosql (redis 6379 ,mongo, 27017) 
+    redis_f = File.open('hosts_redis.txt', 'w')
+    output_by_port(:file, 6379).each { |final_host| redis_f << "#{final_host}\n" }
+    mongo_f = File.open('hosts_mongo.txt', 'w')
+    output_by_port(:file, 27017).each { |final_host| mongo_f << "#{final_host}\n" }
+
   end
 
   # this needs some serious work
@@ -117,9 +175,31 @@ class ParseScan < Nmap::XML
     puts LINE_SEPERATOR
   end
   
+  
+end
 
+## SHELL
+def nmap_shell
+  # need to add the ability to detect OS to get this path
+  list = Dir.entries("/usr/share/nmap/scripts")
+  comp = proc { |s| list.grep(/^#{Regexp.escape(s)}/) }
+
+  Readline.completion_append_character = " "
+  Readline.completion_proc = comp
+  
+  while input = Readline.readline("> ", true)
+    break                       if input == "exit"
+    puts Readline::HISTORY.to_a if input == "hist"
+
+    # Remove blank lines from history
+    Readline::HISTORY.pop if input == ""
+
+    system(input)
+  end
 end
 
 #puts ARGV[0]
-#test = ParseScan.new(ARGV[0])
-#test.live_hosts_to_stdout
+test = ParseScan.new(ARGV[0])
+nmap_shell
+
+
