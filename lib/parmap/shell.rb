3e7c8dc01af5fd4288d70d6b12c1e2a1d9bb7676
Patrick Fussell <patrick.fussell@gmail.com>
2018-06-27 15:51:55 -0700

    updated shell

--- lib/parmap/shell.rb
+++ lib/parmap/shell.rb
@@ -2,6 +2,15 @@
 
 require 'readline'
 
+data_set = false
+prompt_text = ""
+
+# there will be four methods related to the importing
+# of scan data to work with within the shell: create, import, use
+#  set, create, import: set active project for already created project,
+#  create a project, add data to project,
+#  use: parse data from a single nmap xml file
+
 def create(ary_of_args)
   puts "I will create stuff"
   puts "and do stuff to"
@@ -18,6 +27,14 @@ def use
   puts "use file"
 end
 
+def set(xml_file)
+  puts "Setting active xml file"
+end
+
+
+# these are the parsing methods. Before any of these can
+# be used you must either set a project or xml file using
+# above methods
 def list
   puts "list hosts"
 end
@@ -34,23 +51,39 @@ def scan
   "run a scan"
 end
 
+def help
+  puts "I am help text"
+end
+
 def scan(from_shell)
   user_input_ary = from_shell.split
   cmd = user_input_ary.shift
 
-  case cmd
+  case cmd.downcase
   when "create"
     create(user_input_ary)
   when "import"
     import
+  when "list"
+    list
   when "exit"
     exit
+  else
+    help
   end
   
 end
 
+
+#  I'd like to have the shell prompt refelct the xml file or project file
+#  you are working with
+
+prompt = "#{prompt_text}>> "
+
+# going to use readline to handle this part
+# that way we can easily add autocomplete and history
 loop do
-  print "> "
+  print prompt
   scan(gets.chomp)
 end
   
