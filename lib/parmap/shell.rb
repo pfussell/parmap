6a92d8d024f81d9bfd281593ca64c819b97e9e6b
Patrick Fussell <patrick.fussell@gmail.com>
2018-06-27 15:30:25 -0700

    new shell paradigm

--- lib/parmap/shell.rb
+++ lib/parmap/shell.rb
@@ -2,56 +2,55 @@
 
 require 'readline'
 
-module Cli
-
-  base_actions = ["create", "import", "use", "list", "port", "services", "scan"]
-  create_args = []
-  list_args = []
-  port_args = []
-  scan_args = []
-
-
-  def shell
-    # need to add the ability to detect OS and test if the scipt
-    # path is present 
-    list = Dir.entries("/usr/share/nmap/scripts")
-    comp = proc { |s| list.grep(/^#{Regexp.escape(s)}/) }
-
-    Readline.completion_append_character = " "
-    Readline.completion_proc = comp
-
-    while input = Readline.readline("> ", true)
-
-      action = input.split
-
-      case input
-      when ""
-      when "exit"
-        break
-      end
-
-      # Remove blank lines from history
-      Readline::HISTORY.pop if input == ""
-    end
+def create(ary_of_args)
+  puts "I will create stuff"
+  puts "and do stuff to"
+  ary_of_args.each do |a|
+    puts a
   end
-
-
-  # figure out if a project already exists
-  # with the given name
-  def proj_exists?(name)
-  end
-
-  # create the project file
-  # .name.yaml
-  def proj_createj(name)
-    # test to see if the proj already exsists
-    # and throw an error if it does
-  end
-
-  def proj_import
-  end
-
 end
 
+def import
+  puts "I will import files into a project"
+end
 
+def use
+  puts "use file"
+end
 
+def list
+  puts "list hosts"
+end
+
+def port
+  puts "lists hosts with port"
+end
+
+def services
+  puts "services"
+end
+
+def scan
+  "run a scan"
+end
+
+def scan(from_shell)
+  user_input_ary = from_shell.split
+  cmd = user_input_ary.shift
+
+  case cmd
+  when "create"
+    create(user_input_ary)
+  when "import"
+    import
+  when "exit"
+    exit
+  end
+  
+end
+
+loop do
+  print "> "
+  scan(gets.chomp)
+end
+  
