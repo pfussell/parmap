87f325d6bd029cfe867efbcf7d9c41b36b4eb0c6
Patrick Fussell <patrick.fussell@gmail.com>
2018-07-02 08:28:00 -0700

    changed shell methods into a module

--- lib/parmap/shell.rb
+++ lib/parmap/shell.rb
@@ -2,92 +2,98 @@
 
 require 'readline'
 
-data_set = false
-@prompt_text = ""
-# there will be four methods related to the importing
-# of scan data to work with within the shell: create, import, use
-#  set, create, import: set active project for already created project,
-#  create a project, add data to project,
-#  use: parse data from a single nmap xml file
+module Cli
+  data_set = false
+  @@prompt_text = ""
+  # there will be four methods related to the importing
+  # of scan data to work with within the shell: create, import, use
+  #  set, create, import: set active project for already created project,
+  #  create a project, add data to project,
+  #  use: parse data from a single nmap xml file
 
-def create(ary_of_args)
-  puts "I will create stuff"
-  puts "and do stuff to"
-  ary_of_args.each do |a|
-    puts a
+  def self.create(ary_of_args)
+    puts "I will create stuff"
+    puts "and do stuff to"
+    ary_of_args.each do |a|
+      puts a
+    end
   end
-end
 
-def import
-  puts "I will import files into a project"
-end
+  def self.import
+    puts "I will import files into a project"
+  end
 
-def use
-  puts "use file"
-end
+  def self.use
+    puts "use file"
+  end
 
-def set(args_ary)
-  filename = args_ary[0].to_s
-  puts "Setting active xml file: #{filename}"
-  puts "setting prompt"
-  @prompt_text = "#{filename}"
-end
+  def self.set(args_ary)
+    filename = args_ary[0].to_s
+    puts "Setting active xml file: #{filename}"
+    puts "setting prompt"
+    @@prompt_text = "#{filename}"
+  end
 
 
-# these are the parsing methods. Before any of these can
-# be used you must either set a project or xml file using
-# above methods
-def list
-  puts "list hosts"
-end
+  # these are the parsing methods. Before any of these can
+  # be used you must either set a project or xml file using
+  # above methods
+  def self.list
+    puts "list hosts"
+  end
 
-def port
-  puts "lists hosts with port"
-end
+  def self.port
+    puts "lists hosts with port"
+  end
 
-def services
-  puts "services"
-end
+  def self.services
+    puts "services"
+  end
 
-def scan
-  "run a scan"
-end
+  def self.scan
+    "run a scan"
+  end
 
-def help
-  puts "I am help text"
-end
+  def self.help
+    puts "I am help text"
+  end
 
-def scan(from_shell)
-  user_input_ary = from_shell.split
-  cmd = user_input_ary.shift
+  def self.scan(from_shell)
+    user_input_ary = from_shell.split
+    cmd = user_input_ary.shift
 
-  case cmd.downcase
-  when "create"
-    create(user_input_ary)
-  when "import"
-    import
-  when "list"
-    list
-  when "set"
-    set(user_input_ary)
-  when "exit"
-    exit
-  else
-    help
+    case cmd.downcase
+    when "create"
+      create(user_input_ary)
+    when "import"
+      import
+    when "list"
+      list
+    when "set"
+      set(user_input_ary)
+    when "exit"
+      exit
+    else
+      help
+    end
+    
   end
   
+  def self.shell
+    loop do
+      prompt = "#{@@prompt_text}>> "
+      print prompt
+      scan(gets.chomp)
+    end
+  end
+
 end
-
-
 #  I'd like to have the shell prompt refelct the xml file or project file
 #  you are working with
 
 
 # going to use readline to handle this part
 # that way we can easily add autocomplete and history
-loop do
-  prompt = "#{@prompt_text}>> "
-  print prompt
-  scan(gets.chomp)
-end
+
   
+Cli.shell
